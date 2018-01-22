520140f89d50f1f807e067f5453e14fe58096ddc
TheTash <t.kashi.92@gmail.com>
2018-01-22 16:14:12 +0000

    first draft of PrintChunks, need to figure out how to call a function in an arguement of another function

---
+++ PrintChunks.js
@@ -0,0 +1,19 @@
+var http = require('http');
+
+
+function getAndPrintHTMLChunks() {
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step1.html'
+  };
+
+  http.get(requestOptions, function (response) {
+    response.setEncoding('utf-8');
+  });
+  response.on('data', function(data) {
+    console.log('Chunk Received. Length: ' + data.length);
+  });
+  response.end('end', function() {
+    console.log('Repsonse stream complete.');
+  });
+}
